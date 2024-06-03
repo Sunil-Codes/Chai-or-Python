91f81408077329aa39e110b5f41dd8572c650808
Sunil <sk36865@gmail.com>
2024-06-03 12:40:32 +0530

    added a new repo for python learning

---
+++ 01_basics/chai.py
@@ -0,0 +1,8 @@
+from hello_chai import chai
+
+chai("tea")
+
+#NOTES
+# learnt importing code from other file
+#new file created when imported new code form other file that manage byte code when we change or add new code
+# https://youtu.be/3HTKc-ZgZbg?list=PLu71SKxNbfoBsMugTFALhdLlZ5VOqCg2s
---
+++ 01_basics/hello_chai.py
@@ -0,0 +1,15 @@
+print("chai or python")
+
+def chai(n):
+    print(n)
+
+chai('hello chai')
+
+chai_1 = "green Tea"
+chai_2 = "masala chai"
+chai_3 = "coke"
+chai_5 = "no chai"
+chai_6 = "no chai 6"
+
+
+# from importlib import reload
